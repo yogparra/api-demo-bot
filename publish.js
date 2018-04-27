21b9e53fbed452b35d96b2a94b11278424c5ce21
gparra <gparra@minvu.cl>
2018-04-26 23:13:57 -0300

    Proyecto inicial.

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../v1bot.zip');
+var kuduApi = 'https://v1bot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$v1bot';
+var password = '96szRS67WuafTh5bL5WXDkTijpDjRkThZRxddLyrumBcpJsMhkWmBH1G6nCK';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('v1bot publish');
+  } else {
+    console.error('failed to publish v1bot', err);
+  }
+});
